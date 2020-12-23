04dd8293e81306ace9dd1a2005532fb5b4c63b43
Joe Rehfuss <joe.rehfuss@gmail.com>
2020-12-23 09:08:48 -0800

    testing npm publish

---
+++ Manatee.js
@@ -0,0 +1,8 @@
+class Manatee {
+    constructor(setup,punchline){
+        this.setup = setup;
+        this.punchline = punchline;
+    }
+}
+
+module.exports = Manatee;
---
+++ index.js
@@ -0,0 +1,13 @@
+const Manatee = require('./Manatee')
+const manatees = [
+    new Manatee("How do fish unlock their doors?","With their manakeys!"),
+    new Manatee("Who reads the ocean news?","Anchormanatees!"),
+    new Manatee("Who watches sports under the sea?","fanatees!"),
+]
+
+const manateeJokeMethods= {
+    getRandomJoke:function(){
+        return manatees[Math.floor(Math.random()*manatees.length)]
+    }
+}
+
